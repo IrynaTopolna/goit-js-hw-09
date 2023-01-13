986c0889d66e2f4ae1f552cf44b18dc6f21a3e6e
IrynaTopolna <itopolna2808@gmail.com>
2023-01-13 20:06:17 +0200

    Adds JS of task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const refs = {
+  firstDelay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const firstDelay = Number(refs.firstDelay.value);
+  const step = Number(refs.step.value);
+  const amount = Number(refs.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    const delay = firstDelay + step * i;
+    const position = i + 1;
+
+    createPromise({ position, delay }).then(onSuccess).catch(onFailure);
   }
+}
+
+function createPromise({ position, delay }) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+function onSuccess(result) {
+  console.log(result);
+}
+
+function onFailure(error) {
+  console.log(error);
 }
